775328abf87ee5b7599f620d68814cf9a863ce8d
FinnLeh <finnleh5@gmail.com>
2026-01-11 02:02:32 +0000

    Created a StateVector struct.
    The struct is used to make the list of states, which was for now received as a bunch of strings, into a structured list of data. I added a from_values function to the StateVector struct, which extracts data from the received vector of values into the StateVector, and also adjusted the main function to test this functionality. Currently, I simply create a new Vector of flights, then iterate through the entire raw state vector, try to load some flight from the values, and if that worked we push the flight to the list of flights, which we print at the end.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,50 @@ struct OpenSkyResponse {
     states: Vec<Vec<Value>>,
 }
 
+#[derive(Debug)]
+struct StateVector {
+    icao24: String,
+    callsign: String,
+    origin_country: String,
+    longitude: Option<f64>,
+    latitude: Option<f64>,
+    on_ground: bool,
+    velocity: Option<f64>,
+}
+
+impl StateVector {
+    fn from_values(values: &Vec<Value>) -> Option<Self> {
+        // If there is less than 10 values, the array is broken and not usable
+        if values.len() < 10 {
+            return None;
+        }
+
+        // Map based on indices.
+        // .as_str() returns Option<&str>.
+        // .to_string() makes it a real String (Deep Copy/Heap allocation).
+        // unwrap_or() takes a specified String, if it was Null.
+        let icao24 = values[0].as_str().unwrap_or("").to_string();
+        let callsign = values[1].as_str().unwrap_or("").to_string();
+        let origin_country = values[2].as_str().unwrap_or("").to_string();
+
+        // Numbers: .as_f64() returns Option<f64> (i.e., either the value or None if Null).
+        let longitude = values[5].as_f64();
+        let latitude = values[6].as_f64();
+        let on_ground = values[8].as_bool().unwrap_or(false);
+        let velocity = values[9].as_f64();
+
+        Some(StateVector {
+            icao24,
+            callsign,
+            origin_country,
+            longitude,
+            latitude,
+            on_ground,
+            velocity,
+        })
+    }
+}
+
 fn main() {
     let raw_data = r#"
     {
@@ -19,5 +63,13 @@ fn main() {
 
     let response: OpenSkyResponse = serde_json::from_str(raw_data).unwrap();
 
-    println!("{:?}", response);
+    let mut flights = Vec::new();
+
+    for raw_state in response.states {
+        if let Some(flight) = StateVector::from_values(&raw_state) {
+            flights.push(flight);
+        }
+    }
+
+    println!("{:?}", flights);
 }
