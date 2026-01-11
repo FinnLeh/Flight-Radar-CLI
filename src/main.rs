4b554910a7d04a1f9e3ec78ddacb9e0f974bcbeb
FinnLeh <finnleh5@gmail.com>
2026-01-11 01:03:45 +0000

    Edited main.rs to contain a test struct for the API data.
    main.rs now contains a struct to store the Open Sky API data, as well as a main function to test with a test raw data string how the serde_json crate works.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use serde::Deserialize;
+use serde_json::Value;
+
+#[derive(Debug, Deserialize)]
+struct OpenSkyResponse {
+    time: i64,
+    states: Vec<Vec<Value>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw_data = r#"
+    {
+        "time": 1683100000,
+        "states": [
+            ["4b1814", "HB-JWC", "Switzerland", 1620000000, 1620000000, 7.5, 47.5, 10000.5, false, 200.0]
+        ]
+    }
+    "#;
+
+    let response: OpenSkyResponse = serde_json::from_str(raw_data).unwrap();
+
+    println!("{:?}", response);
 }
